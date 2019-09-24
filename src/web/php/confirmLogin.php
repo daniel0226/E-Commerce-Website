6d6f94c90f7cd4259dcf7f7b8b57abb911a60e0e
daniel0226 <daniel1996k@icloud.com>
2019-09-24 13:01:28 -0400

    confirmLogin php comments

--- src/web/php/confirmLogin.php
+++ src/web/php/confirmLogin.php
@@ -1,9 +1,24 @@
 <?php 
+session_start();
 
 //Global variables
 
 $loginType = ""; //Keep track if user is normal web user or administrator
 
+
+/**
+ * 
+ * Will have to change code and function depending on database
+ * If we have separate database for admin or user information
+ * Or if we have a table cell labeling a user as admin or user.
+ * 
+ */
+
+/**
+ * Connect to MySQL database
+ * Validate Login information
+ * Update LoginType (E.G user/admin)
+ */
 function confirmLogin()
 {
     $validLogin = FALSE;
@@ -12,13 +27,35 @@ function confirmLogin()
     $username = $_GET["username"];
     $password = $_GET["password"];
 
-    //Connect to DB
-    //Validate login info
-    //update logintype
+    $database = connectToDatabase();
+
+    $databaseUsername = mysqli_real_escape_string($database, $username);
+    $databasePassword = mysqli_real_escape_string($database, $password);
+
+    $validLogin
+
     $loginType = "admin";
     
 
     return $validLogin
+}
+
+/**
+ * Connect to database
+ */
+function connectToDatabase()
+{
+    //Credentials to login to database
+    $server = "";
+    $username = "";
+    $password = "";
+    $databaseName = "";
+    $database = mysqli_connect($server, $username, $password, $databaseName);
+    
+    //Possibly handle mysql security stuff
+    //like prevent mysql injections
+    
+    return $database;
 }
 
 function validateLogin($validLogin)
