6c10ed3d08b5a9555191179b2ac258f12f166bd1
daniel0226 <daniel1996k@icloud.com>
2019-10-02 20:37:03 -0400

    Finished edit profile UI

--- src/web/js/fields.js
+++ src/web/js/fields.js
@@ -17,6 +17,22 @@ function searchNav(type)
         //location.replace("src/web/html/searchView.html");
     }else{
         //location.replace("../html/SearchView.html");
+    }   
+}
+
+function openTab(event, TabID)
+{
+    var i, tabBody, tabLinks;
+    tabBody = document.getElementsByClassName("tabcontent");
+    for(i = 0; i<tabBody.length; i++)
+    {
+        tabBody[i].style.display = "none";
     }
-    
+    tabLinks = document.getElementsByClassName("tablinks");
+    for(i = 0; i<tabLinks.length; i++)
+    {
+        tabLinks[i].className = tabLinks[i].className.replace("tabActive", "");
+    }
+    document.getElementById(TabID).style.display = "block";
+    event.currentTarget.className += " tabActive";
 }
