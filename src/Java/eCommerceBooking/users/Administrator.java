8811b6a0454bf1956c83de30c11bd21d6bb013ac
daniel0226 <daniel1996k@icloud.com>
2019-10-02 22:01:34 -0400

    Basic java stuff

---
+++ src/Java/eCommerceBooking/users/Administrator.java
@@ -0,0 +1,9 @@
+package eCommerceBooking.users;
+
+public class Administrator extends Users
+{
+    public Administrator(String fName, String lName)
+    {
+        super(fName, lName);
+    }
+}
---
+++ src/Java/eCommerceBooking/users/WebUser.java
@@ -0,0 +1,11 @@
+package eCommerceBooking.users;
+
+public class WebUser extends Users
+{
+
+    public WebUser(String fName, String lName)
+    {
+        super(fName, lName);
+    }
+
+}
