2c32da6d257f1bfe172a09fdba954a6dc44edb04
daniel0226 <daniel1996k@icloud.com>
2019-10-29 13:11:06 -0400

    Renamed email

--- src/eCommerce/Controllers/EmailController.java
+++ src/eCommerce/Controllers/EmailController.java
@@ -1,49 +1,94 @@
-	import javax.mail.Message;
-	import javax.mail.MessagingException;
-	import javax.mail.PasswordAuthentication;
-	import javax.mail.Session;
-	import javax.mail.Transport;
-	import javax.mail.internet.InternetAddress;
-	import javax.mail.internet.MimeMessage;
-	import java.util.Properties;
+package eCommerce.Controllers;
 
-	public class EmailController{
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-	    public static void main(String[] args) {
+import eCommerce.Database.Database;
+import eCommerce.users.WebUser;
 
-	        final String username = "bookamovie1@gmail.com";
-	        final String password = "Movie@gmail123";
+import java.io.IOException;
+import java.util.Properties;
+import eCommerce.Strings.email;
 
-	        Properties prop = new Properties();
-	        prop.put("mail.smtp.host", "smtp.gmail.com");
-	        prop.put("mail.smtp.port", "587");
-	        prop.put("mail.smtp.auth", "true");
-	        prop.put("mail.smtp.starttls.enable", "true");
 
-	        Session session = Session.getInstance(prop,
-	                new javax.mail.Authenticator() {
-	                    protected PasswordAuthentication getPasswordAuthentication() {
-	                        return new PasswordAuthentication(username, password);
-	                    }
-	                });
+@WebServlet("/EmailController")
+public class EmailController extends HttpServlet
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	final private String username = "bookamovie1@gmail.com";
+    final private String password = "Movie@gmail123";
+    Properties prop = null;
 
-	        try {
+    //If class is called by jsp
+    public void init()
+    {
+    	prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+    }
+    
+    //If class is created 
+	public EmailController()
+	{
+		prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+	}
+	
 
-	            Message message = new MimeMessage(session);
-	            message.setFrom(new InternetAddress("bookamovie1@gmail.com"));
-	            message.setRecipients(
-	                    Message.RecipientType.TO,
-	                    InternetAddress.parse("kranthi7910@gmail.com"));
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {	
+		String emailToSendConfirmation = request.getParameter("email");
+		WebUser user = Database.getUser(emailToSendConfirmation);
+		String reconfirm = email.reconfirmMsg + " http://localhost:8080/cinemaBooking/registerConfirmation.jsp?email=" + user.getEmail()+"&code="+user.getCode();
+		System.out.println(reconfirm);
+		sendEmail(user, email.confirmMsg, reconfirm);
+		request.setAttribute("loginError", email.confirmationSent);
+		request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+	
+	public void sendEmail(WebUser user, String subject, String emailMsg)
+	{
+		Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
 
-	            message.setSubject("Testing Javamail via Gmail TLS");
-	            message.setText("Dear Registered User,"
-	                    + "\n\n This is your activation link. It expires in 24 hours!");
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("bookamovie1@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(user.getEmail()));
 
-	            Transport.send(message, message.getAllRecipients());
+            message.setSubject(subject);
+            message.setText(emailMsg);
 
-	            System.out.println("Sent email successfully!");
+            Transport.send(message, message.getAllRecipients());
 
-	        } catch (MessagingException e) {
-	            e.printStackTrace();
-	        }
-	    }
+            System.out.println("Sent email successfully!");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+	}
+}
